79662b2cbad3400cd3de6c92a7615dd69bff3b40
nikhilghadi <nikhilghadi665@gmail.com>
2026-04-16 21:36:12 +0530

    country parameter validation added for API end point

--- backend/app/controllers/insights_controller.rb
+++ backend/app/controllers/insights_controller.rb
@@ -1,5 +1,8 @@
 class InsightsController < ApplicationController
   def country
+    if params[:country].blank?
+      return render json: { error: "country parameter is required" }, status: :unprocessable_entity
+    end
     insights = SalaryInsights.for_country(params[:country])
     render json: insights
   end
