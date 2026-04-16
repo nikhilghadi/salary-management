d4b3353af2f703f640edefdb48b42e07ffcef216
nikhilghadi <nikhilghadi665@gmail.com>
2026-04-16 08:51:23 +0530

    signle field validation checking test cases

--- backend/spec/models/employee_spec.rb
+++ backend/spec/models/employee_spec.rb
@@ -15,4 +15,32 @@ RSpec.describe Employee, type: :model do
 
     expect(employee).to be_valid
   end
+
+  it "is invalid without emp_code" do
+    employee = Employee.new(
+      first_name: "Nikhil",
+      last_name: "Ghadi",
+      job_title: "Engineer",
+      country: "India",
+      department: "Engineering",
+      branch_location: "Bangalore",
+      salary: 50000
+    )
+
+    expect(employee).not_to be_valid
+  end
+
+  it "is invalid without salary" do
+    employee = Employee.new(
+      first_name: "Nikhil",
+      last_name: "Ghadi",
+      emp_code: "E12345",
+      job_title: "Engineer",
+      country: "India",
+      department: "Engineering",
+      branch_location: "Bangalore",
+    )
+
+    expect(employee).not_to be_valid
+  end
 end
